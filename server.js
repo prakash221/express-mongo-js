1013d16a2bafd7b78cccbb029d7f1fcf2e8e0e3b
Prakash Sharma <prakash.psc221@gmail.com>
2024-10-21 21:34:27 +0545

    feat: Add cors package for cross-origin resource sharing

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ import connectDB from './config/db.js'
 import authRoutes from './routes/authRoute.js'
 import userRoutes from './routes/userRoute.js'
 import errorHandler from './middlewares/errorMiddleware.js'
-
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
 connectDB()
 
+app.use(
+	cors({
+		origin: '*',
+	})
+)
 app.use(express.json())
 app.get('/', (req, res) => res.send('API is running'))
 app.get('/api', (req, res) => res.send({ data: 'API is running' }))
